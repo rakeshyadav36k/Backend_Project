5792a9598416d9860fb44f46a9e7d9680739d02b
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-09-15 00:12:20 +0530

    Create auth.routes.js

---
+++ eCommerce_project/routes/auth.routes.js
@@ -0,0 +1,19 @@
+/**
+ * POST localhost:8080/ecomm/api/v1/auth/signup
+ * POST 127.0.0.1:8080/ecomm/api/v1/auth/signup
+ * 
+ * I need to intercept this
+ */
+
+const authController = require("../controllers/auth.controller")
+const authMW = require("../middlewares/auth.mw")
+
+module.exports = (app)=>{
+    app.post("/ecomm/api/v1/auth/signup",[authMW.verifySignUpBody], authController.signup)
+
+    /**
+     * route for
+     * POST localhost:8080/ecomm/api/v1/auth/signin
+     */
+    app.post("/ecomm/api/v1/auth/signin", authController.signin)
+}
